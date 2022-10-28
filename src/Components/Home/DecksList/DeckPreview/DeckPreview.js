db78822a230c3100a4f0c93b4b3296cfebdbbef8
Théo <tduval@student.42.fr>
2022-10-28 19:30:36 +0200

    added a tooltip on deck names for when the name overflows on x axis

--- src/Components/Home/DecksList/DeckPreview/DeckPreview.js
+++ src/Components/Home/DecksList/DeckPreview/DeckPreview.js
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+import ReactTooltip from "react-tooltip";
 
 const DeckPreview = ({ deck }) => {
   return (
     <Link to={`/deck/${deck.id}`}>
-      <button className="w-64 overflow-hidden bg-gray-600 text-white text-4xl rounded-lg px-4 py-2">
+      <button
+        data-tip={deck.name}
+        className="w-64 overflow-hidden bg-gray-600 text-white text-4xl rounded-lg px-4 py-2"
+      >
         {deck.name}
       </button>
+      <ReactTooltip />
     </Link>
   );
 };

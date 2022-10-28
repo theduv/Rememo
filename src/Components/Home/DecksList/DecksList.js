14eed353daf894f207f4958e73e97baf885a8822
Théo <tduval@student.42.fr>
2022-10-28 23:02:48 +0200

    testing changes on json

--- src/Components/Home/DecksList/DecksList.js
+++ src/Components/Home/DecksList/DecksList.js
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
 import DeckPreview from "./DeckPreview/DeckPreview";
 
 const DecksList = ({ decks }) => {
   return (
     <div className="flex flex-col justify-center items-center space-y-4">
-      {decks.map((deck) => (
-        <DeckPreview deck={deck} />
-      ))}
+      {decks.length ? (
+        decks.map((deck) => <DeckPreview deck={deck} />)
+      ) : (
+        <div>
+          You didn't create a deck yet.{" "}
+          <Link to="/decks" className="text-blue-400">
+            Go create one!
+          </Link>
+        </div>
+      )}
     </div>
   );
 };

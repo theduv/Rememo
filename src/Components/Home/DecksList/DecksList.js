da230e08d3527b3492e9e0558b58c10c5717ab8e
Théo <tduval@student.42.fr>
2022-10-28 19:27:24 +0200

    added some spacing to the decks list

--- src/Components/Home/DecksList/DecksList.js
+++ src/Components/Home/DecksList/DecksList.js
@@ -3,7 +3,7 @@ import DeckPreview from "./DeckPreview/DeckPreview";
 
 const DecksList = ({ decks }) => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center space-x-4">
       {decks.map((deck) => (
         <DeckPreview deck={deck} />
       ))}

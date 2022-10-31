fa2a6ef30f3f5842eb95cea293418dad718be08c
Théo <tduval@student.42.fr>
2022-10-31 16:16:54 +0100

    centered text on front card

--- src/Components/SingleDeck/SingleCard/SingleCard.js
+++ src/Components/SingleDeck/SingleCard/SingleCard.js
@@ -21,10 +21,10 @@ const SingleCard = ({
   return (
     <div className="flex flex-col space-y-12 items-center w-full h-full">
       <div className="border-2 border-gray-400 rounded-lg p-6 w-1/5 flex flex-col justify-between h-96">
-        <h1 className="m-auto text-3xl">{cardData.front}</h1>
+        <h1 className="m-auto text-3xl text-center">{cardData.front}</h1>
         <div className="border w-full border-gray-300" />
         {clickedShow ? (
-          <h1 className="m-auto text-3xl">{cardData.back}</h1>
+          <h1 className="m-auto text-3xl text-center ">{cardData.back}</h1>
         ) : (
           <button onClick={onClickShow} className="m-auto italic text-3xl">
             Show answer

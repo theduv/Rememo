bd7e2b95aaa143d1c5cf224a0602251b9a6119c2
Théo <tduval@student.42.fr>
2022-11-08 22:44:03 +0100

    changed the font size of the decks on home

--- src/Components/Home/DecksList/DeckPreview/DeckPreview.tsx
+++ src/Components/Home/DecksList/DeckPreview/DeckPreview.tsx
@@ -12,12 +12,12 @@ const DeckPreview = ({ deck }: DeckPreviewProps) => {
     <Link to={`/deck/${deck.id}`}>
       <button
         data-tip={deck.name}
-        className="w-64 flex items-center justify-between overflow-ellipsis bg-gray-600 text-white text-2xl rounded-lg px-4 py-2"
+        className="w-96 flex text-4xl items-center justify-between overflow-ellipsis bg-gray-600 text-white rounded-lg px-4 py-2"
       >
         {deck.name}
         <div className="flex items-center space-x-2 text-base">
-          <Book height={20} width={20} />
-          <h1 className="text-xl">{deck.numberOfCards}</h1>
+          <Book height={30} width={30} />
+          <h1 className="text-4xl">{deck.numberOfCards}</h1>
         </div>
       </button>
       <ReactTooltip />
